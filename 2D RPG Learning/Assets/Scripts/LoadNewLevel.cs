f9ef98d1f579f06c2255146dcf94efec78b6ec2d
Rafael Bueno <racabu@gmail.com>
2016-11-05 23:36:21 -0600

    Add files via upload

---
+++ 2D RPG Learning/Assets/Scripts/LoadNewLevel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNewLevel : MonoBehaviour {
+
+    public string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Application.LoadLevel(levelToLoad);
+        }
+    }
+}
